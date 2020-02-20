c6c9e1a82fe72a68f15b2d9d1a3cef651e91dd43
NekoNikon <nikon9nikon@gmail.com>
2020-02-20 16:50:51 +0600

    ship

--- Player.java
+++ Player.java
@@ -1,6 +1,10 @@
 
 class Player {
+    private boolean is_bot;
+    private String name;
+    private Ship[] ships = new Ship[10];
     public Player(boolean is_bot , String name) {
-        
+        this.is_bot = is_bot;
+        this.name = name;
     }
 }
--- Ship.java
+++ Ship.java
@@ -1,3 +1,12 @@
+import java.util.LinkedList;
+
+class TypeShip { 
+    public int BOAT = 1;
+    public int DESTROYER = 2;
+    public int CRUISER = 3;
+    public int BATTLSESHIP = 4;
+}
+
 class Coord {
     public int x;
     public int y;
@@ -6,5 +15,10 @@ class Coord {
 
 
 class Ship {
+    public LinkedList<Coord> coords;
+    public TypeShip type;
+    
+    public void addCarr(Coord coord) {
 
+    }
 }
